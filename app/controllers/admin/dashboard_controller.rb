38dd80289bd2fcb52f66a3a19495cd35d6741a08
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-06-24 04:49:01 +0000

    fixed admin security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['SOME_USER'], password: ENV['SOME_PASSWORD']
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['SOME_USER'], password: ENV['SOME_PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
