2a8ea9b846444e0a9339d4d86811b5398024c656
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-06-24 05:10:35 +0000

    improved admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['SOME_USER'], password: ENV['SOME_PASSWORD']
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
